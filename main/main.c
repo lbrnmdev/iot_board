b7418fc5c93402e2719978a2ea0523fac79c6bcd
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2019-11-06 20:54:36 +0100

    setup wifi connectivity

--- main/main.c
+++ main/main.c
@@ -1,6 +1,75 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+
+#include "esp_log.h"
+
+//Define this variable to be used in logging macros eg ESP_LOGI(tag, format, ...)
+static const char *TAG = "IOT_BOARD";
+
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+{
+  switch (event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+      esp_wifi_connect();
+      break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+      xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      esp_wifi_connect();
+      xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+      break;
+    default:
+      break;
+  }
+  return ESP_OK;
+}
+
+static void wifi_init(void)
+{
+  tcpip_adapter_init();
+  wifi_event_group = xEventGroupCreate();
+  ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  wifi_config_t wifi_config = {
+    .sta = {
+      .ssid = CONFIG_WIFI_SSID,
+      .password = CONFIG_WIFI_PASSWORD,
+    },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_LOGI(TAG, "start the WIFI SSID:[%s]", CONFIG_WIFI_SSID);
+  ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_LOGI(TAG, "Waiting for wifi");
+  xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+}
 
 void app_main(void)
 {
-  
+  ESP_LOGI(TAG, "[APP] Startup..");
+  ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+  esp_log_level_set("*", ESP_LOG_INFO);
+
+  nvs_flash_init();
+  wifi_init();
 }
